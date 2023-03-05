1371c819a100434a6a1a6f1dcc56e7b6d5332dc8
ahmetozdemr <ahmetzdmiir@gmail.com>
2023-03-06 01:48:58 +0300

    prduct api aded

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -3,14 +3,21 @@ import { Product } from 'src/app/models/product';
 import { HttpClient } from '@angular/common/http';
 import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
+
+
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  products: Product[] = [];
-  apiUrl = 'https://localhost:44314/api/products/getall';
+
+  products:any = [];
+
+  //apiUrl = 'https://localhost:44381/api/products/getall';
+  apiUrl = 'https://demodata.grapecity.com/northwind/api/v1/Products';
+   
   constructor(private httpClient: HttpClient) {}
 
   ngOnInit(): void {
@@ -19,9 +26,29 @@ export class ProductComponent implements OnInit {
 
   getProducts() {
     this.httpClient
-      .get<ProductResponseModel>(this.apiUrl)
+      .get(this.apiUrl)
       .subscribe((response) => {
-        this.products = response.data;
+        this.products = response;
       });
   }
 }
+
+/*   todos:any = [];
+  apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.httpClient
+      .get(this.apiUrl)
+      .subscribe((response) => {
+        this.todos = response;
+      });
+  }
+} */
+
+ 
