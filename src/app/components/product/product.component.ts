2e74bf0148d40000ff7668864c5e16b603e34ea7
ahmetozdemr <ahmetzdmiir@gmail.com>
2023-03-05 03:32:38 +0300

    export interface

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,42 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
-  product1 = {
-    productId: 1,
-    productName: 'Bardak',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product2 = {
-    productId: 1,
-    productName: 'Laptop',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product3 = {
-    productId: 1,
-    productName: 'Mouse',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product4 = {
-    productId: 1,
-    productName: 'Keyboard',
-    categoryId: 1,
-    unitPrice: 5,
-  };
-  product5 = {
-    productId: 1,
-    productName: 'Camera',
-    categoryId: 1,
-    unitPrice: 5,
-  };
 
-  products = [
-    this.product1,
-    this.product2,
-    this.product3,
-    this.product4,
-    this.product5,
+
+  products:Product[] = [
+
   ];
 }
